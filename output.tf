37830e3fe678d04ad3b3445da913f224ba1c4df1
Sami Islam <islam.samipkr@gmail.com>
2023-07-02 14:52:17 -0400

    2

--- output.tf
+++ output.tf
@@ -1,10 +1,6 @@
 output "cloud_armor_policies"{
     value=local.cloud_armor_policies
 }
-output "pre_configured_rules" {
-    value=local.cloud_armor_policies
-    
-}
 output "cloud_armor_list"{
     value=local.cloud_armor_list
 }
