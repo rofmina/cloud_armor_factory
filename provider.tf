b0b0855ad80e20a2e175e19cac3bd5b3796d0130
Sami Islam <islam.samipkr@gmail.com>
2023-07-02 14:27:30 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,14 @@
+terraform {
+  required_providers {
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 4.0"
+    }
+  }
+}
 provider "google" {
-  project_id = var.project_id
-  region  = var.region
 }
 
 provider "google-beta" {
-  project_id = var.project_id
-  region  = var.region
 }
+
