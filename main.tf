f88e5ecfc166376c17776431e701f4f7b5d3c7bd
Sami Islam <islam.samipkr@gmail.com>
2023-07-03 10:36:58 -0400

    setting up pre configured rules

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ module "cloud_armor" {
       target_rule_set = "sqli-v33-stable"
       sensitivity_level  = 4
     }
-
+/*
     "xss_level2" = {
       action                  = "deny(502)"
       priority                = 2
@@ -139,7 +139,7 @@ module "cloud_armor" {
       description             = "CVEs sensitivity level 3"
       target_rule_set         = "cve-canary"
       sensitivity_level       = 3
-    }
+    }*/
 
   }
 }
