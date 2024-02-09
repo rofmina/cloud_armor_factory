3317db448c3d424aff7c883e5de2127db4892796
Sami Islam <islam.samipkr@gmail.com>
2024-02-09 10:55:14 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,19 @@
+/*
 terraform {
   cloud {
     organization = "sami600"
 
     workspaces {
       name = "cloud_armor_factory"
+    }
+  }
+}*/
+terraform {
+  cloud {
+    organization = "sami123123123"
+
+    workspaces {
+      name = "cloud_armor_factory_mcit_8thFeb"
     }
   }
 }
