dc6583c2b83720a0669b8c89388f42ef61b7dde9
Sami Islam <islam.samipkr@gmail.com>
2024-02-14 20:22:25 -0500

    Update service_account_module.tf

--- service_account_module.tf
+++ service_account_module.tf
@@ -7,3 +7,26 @@ module "service_accounts" {
   display_name  = "Single Account"
   description   = "Single Account Description"
 }
+variable "service_accounts_yaml" {
+  description = "Path to the YAML file containing service accounts"
+}
+output "service_accounts" {
+  description = "The imported service accounts"
+  value       = module.service_accounts1
+}
+
+variable "service_accounts_yaml" {
+  description = "Path to the YAML file containing service accounts"
+}
+
+locals {
+  service_accounts = yamldecode(file(var.service_accounts_yaml)).service_accounts
+}
+
+module "service_accounts1" {
+  source  = "terraform-google-modules/service-accounts/google"
+
+  for_each            = { for idx, sa in local.service_accounts : idx => sa }
+
+  account_id          = each.value.name
+  project_id          = each.value.project_id
