7b0488c106193318cba60abbacf8ba6e36de6d30
rofmina <119059458+rofmina@users.noreply.github.com>
2024-06-10 19:45:06 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -10,7 +10,7 @@ terraform {
 }*/
 terraform {
   cloud {
-    organization = "sami123123123"
+    organization = "mina_cloud_armor_factory"
 
     workspaces {
       name = "cloud_armor_factory_mcit_8thFeb"
